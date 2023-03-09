e60336366ef4c064fee6ddf72aa031f42c2fca4c
Osariemen Daniel Osazee <68135656+thexovc@users.noreply.github.com>
2023-03-09 11:17:04 +0100

    testing for vulnerability

--- smart-contract/test/Xtelpt.js
+++ smart-contract/test/Xtelpt.js
@@ -11,13 +11,13 @@ describe("XTELPT Unit Tests", function () {
         owner = accounts[0].address
         account = accounts[1].address
         xtelptContract = await ethers.getContractFactory("XTELPT")
-        xtelpt = await xtelptContract.deploy()
+        XTELPT = await xtelptContract.deploy()
         await xtelpt.deployed()
     })
 
     it("Checks the owner", async () => {
         try {
-            let result = await xtelpt.owner()
+            let result = await XTELPT.owner()
             console.log("result working", result.toString())
             assert.equal(result.toString(), owner)
         } catch (e) {
@@ -27,7 +27,7 @@ describe("XTELPT Unit Tests", function () {
 
     it("Initial meeting num is 0", async () => {
         try {
-            let result = await xtelpt.meetingNum()
+            let result = await XTELPT.meetingNum()
             assert.equal(result.toString(), "0")
         } catch (e) {
             console.log("meeting num is not 0")
@@ -36,12 +36,48 @@ describe("XTELPT Unit Tests", function () {
 
     it("Checks that Intial Accounts is an empty array", async () => {
         try {
-            let result = await xtelpt.AllAccount()
+            let result = await XTELPT.AllAccount()
             assert.equal(result, [])
         } catch (e) {
             console.log("Accounts is not an empty array")
         }
     })
+    
+    describe("createUser", function () {
+    it("should set user profile", async function () {
+      const rating = 4;
+      const name = "Alice";
+      const pic = "https://example.com/alice.jpg";
+      const bio = "Hi, I'm Alice!";
+      await XTELPT.createUser(rating, name, pic, bio);
+      const userProfile = await XTELPT.UserProfile(await ethers.provider.getSigner());
+      expect(userProfile.addr).to.equal(await ethers.provider.getSigner().getAddress());
+      expect(userProfile.name).to.equal(name);
+      expect(userProfile.rating).to.equal(rating);
+      expect(userProfile.role).to.equal("User");
+      expect(userProfile.profilePic).to.equal(pic);
+      expect(userProfile.bio).to.equal(bio);
+    });
+  });
+
+  describe("createHost", function () {
+    it("should set host profile", async function () {
+      const rating = 5;
+      const name = "Bob";
+      const pic = "https://example.com/bob.jpg";
+      const bio = "Hi, I'm Bob!";
+      const title = "Awesome Host";
+      await XTELPT.createHost(rating, name, pic, bio, title);
+      const hostProfile = await XTELPT.UserProfile(await ethers.provider.getSigner());
+      expect(hostProfile.addr).to.equal(await ethers.provider.getSigner().getAddress());
+      expect(hostProfile.name).to.equal(name);
+      expect(hostProfile.rating).to.equal(rating);
+      expect(hostProfile.role).to.equal("Host");
+      expect(hostProfile.profilePic).to.equal(pic);
+      expect(hostProfile.bio).to.equal(bio);
+      expect(hostProfile.hostTitle).to.equal(title);
+    });
+  });
 
 
 })
